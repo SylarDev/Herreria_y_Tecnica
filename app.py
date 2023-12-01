d3d684855edcf580068ebbb18c81500553d84e63
SylarDev1 <maximo252@gmail.com>
2023-11-30 21:00:17 -0300

    modProducto

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 #--------------------------------------------------------------------
 from flask import Flask, request, jsonify, render_template
-from flask import request, redirect, url_for
+from flask import redirect, url_for
 
 # Instalar con pip install flask-cors
 from flask_cors import CORS
@@ -62,19 +62,19 @@ class Catalogo:
         self.cursor = self.conn.cursor(dictionary=True)
         
     #----------------------------------------------------------------
-    def agregar_producto(self, codigo, descripcion, cantidad, precio, imagen, proveedor):
+    def agregar_producto(self, codigo, descripcion, cantidad, precio, proveedor):
         # Verificamos si ya existe un producto con el mismo código
         self.cursor.execute(f"SELECT * FROM productos WHERE codigo = {codigo}")
         producto_existe = self.cursor.fetchone()
         if producto_existe:
             return False
 
-        sql = "INSERT INTO productos (codigo, descripcion, cantidad, precio, imagen_url, proveedor) VALUES (%s, %s, %s, %s, %s, %s)"
-        valores = (codigo, descripcion, cantidad, precio, imagen, proveedor)
+        sql = "INSERT INTO productos (codigo, descripcion, cantidad, precio, proveedor) VALUES (%s, %s, %s, %s, %s)"
+        valores = (codigo, descripcion, cantidad, precio, proveedor)
 
         self.cursor.execute(sql, valores)        
         self.conn.commit()
-        return True
+        return self.cursor.rowcount > 0
 
     #----------------------------------------------------------------
 
@@ -84,13 +84,15 @@ class Catalogo:
         return self.cursor.fetchone()
 
     #----------------------------------------------------------------
-    def modificar_producto(self, codigo, nueva_descripcion, nueva_cantidad, nuevo_precio, nueva_imagen, nuevo_proveedor):
-        sql = "UPDATE productos SET descripcion = %s, cantidad = %s, precio = %s, imagen_url = %s, proveedor = %s WHERE codigo = %s"
-        valores = (nueva_descripcion, nueva_cantidad, nuevo_precio, nueva_imagen, nuevo_proveedor, codigo)
+    def modificar_producto(self, codigo, nueva_descripcion, nueva_cantidad, nuevo_precio, nuevo_proveedor):
+        sql = "UPDATE productos SET descripcion = %s, cantidad = %s, precio = %s, proveedor = %s WHERE codigo = %s"
+        valores = (nueva_descripcion, nueva_cantidad, nuevo_precio, nuevo_proveedor, codigo)
         self.cursor.execute(sql, valores)
         self.conn.commit()
+        
         return self.cursor.rowcount > 0
-
+    
+    
     #----------------------------------------------------------------
     def listar_productos(self):
         self.cursor.execute("SELECT * FROM productos")
@@ -114,12 +116,11 @@ class Catalogo:
             print(f"Descripción: {producto['descripcion']}")
             print(f"Cantidad...: {producto['cantidad']}")
             print(f"Precio.....: {producto['precio']}")
-            print(f"Imagen.....: {producto['imagen_url']}")
+            
             print(f"Proveedor..: {producto['proveedor']}")
             print("-" * 40)
         else:
             print("Producto no encontrado.")
-
 
 #--------------------------------------------------------------------
 # Cuerpo del programa
@@ -135,7 +136,13 @@ def productos():
     return render_template('productos.html')
 
 # Crear una instancia de la clase Catalogo
+
 catalogo = Catalogo(host='localhost', user='root', password='Rodrigo2023', database='miapp')
+
+#print(catalogo.modificar_producto(2, 'osito', 10, 2332, 5))
+
+
+
 
 # Carpeta para guardar las imagenes.
 ruta_destino = './static/img/'
@@ -152,56 +159,54 @@ def mostrar_producto(codigo):
     if producto:
         return jsonify(producto)
     else:
-        return "Producto no encontrado", 404, render_template('productos.html')
+        return "Producto no encontrado", 404
 
 #--------------------------------------------------------------------
 
 @app.route("/productos", methods=["POST"])
 def agregar_producto():
-    codigo = request.form['codigo']
-    descripcion = request.form['descripcion']
-    cantidad = request.form['cantidad']
-    precio = request.form['precio']
-    proveedor = request.form['proveedor']  
-    imagen = request.files['imagen']
-    nombre_imagen = secure_filename(imagen.filename)
     
-    nombre_base, extension = os.path.splitext(nombre_imagen)
-    nombre_imagen = f"{nombre_base}_{int(time.time())}{extension}"
-    imagen.save(os.path.join(ruta_destino, nombre_imagen))
-
-    if catalogo.agregar_producto(codigo, descripcion, cantidad, precio, nombre_imagen, proveedor):
+    codigo = request.form.get('codigo')
+    descripcion = request.form.get('descripcion')
+    cantidad = request.form.get('cantidad')
+    precio = request.form.get('precio')
+    proveedor = request.form.get('proveedor')  
+    
+    if catalogo.agregar_producto(codigo, descripcion, cantidad, precio, proveedor):
         return jsonify({"mensaje": "Producto agregado"}), 201
     else:
         return jsonify({"mensaje": "Producto ya existe"}), 400
 
 #--------------------------------------------------------------------
-
 @app.route("/productos/<int:codigo>", methods=["PUT"])
 def modificar_producto(codigo):
-    # Procesamiento de la imagen
-    imagen = request.files['imagen']
-    nombre_imagen = secure_filename(imagen.filename)
-    print("*"*20)
-    print(nombre_imagen)
-    print("*"*20)
-    nombre_base, extension = os.path.splitext(nombre_imagen)
-    nombre_imagen = f"{nombre_base}_{int(time.time())}{extension}"
-    imagen.save(os.path.join(ruta_destino, nombre_imagen))
+    try:
+        print(f"DEBUG: Código recibido: {codigo}")
 
-    # Datos del producto
-    data = request.form
-    nueva_descripcion = data.get("descripcion")
-    nueva_cantidad = data.get("cantidad")
-    nuevo_precio = data.get("precio")
-    nuevo_proveedor = data.get("proveedor")
+        nueva_descripcion = request.form.get('descripcion')
+        nueva_cantidad = request.form.get('cantidad')
+        nuevo_precio = request.form.get('precio')
+        nuevo_proveedor = request.form.get('proveedor')
 
-    # Actualización del producto
-    if catalogo.modificar_producto(codigo, nueva_descripcion, nueva_cantidad, nuevo_precio, nombre_imagen, nuevo_proveedor):
-        return jsonify({"mensaje": "Producto modificado"}), 200
-    else:
-        return jsonify({"mensaje": "Producto no encontrado"}), 404
+        # Validar los datos de entrada
+        if not codigo or not nueva_cantidad or not nuevo_precio or not nuevo_proveedor:
+            return jsonify({"mensaje": "Datos de entrada incompletos"}), 400
 
+        # Convertir valores según sea necesario (por ejemplo, a números)
+        nueva_cantidad = int(nueva_cantidad)
+        nuevo_precio = float(nuevo_precio)
+        nuevo_proveedor = int(nuevo_proveedor)
+
+        if catalogo.modificar_producto(codigo, nueva_descripcion, nueva_cantidad, nuevo_precio, nuevo_proveedor):
+            return jsonify({"mensaje": "Producto modificado"}), 200
+        else:
+            return jsonify({"mensaje": "Producto no encontrado"}), 404
+    except mysql.connector.Error as err:
+        print(f"Error de MySQL: {err}")
+        return jsonify({"mensaje": "Error de MySQL"}), 500
+    except Exception as e:
+        print(f"Error interno: {e}")
+        return jsonify({"mensaje": f"Error interno: {str(e)}"}), 500
 
 #--------------------------------------------------------------------
 
