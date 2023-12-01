b25ce727a23a8ad26d7f280cab3d0cfaa4103856
SylarDev1 <maximo252@gmail.com>
2023-12-01 18:41:41 -0300

    ultima modificacion

--- app.py
+++ app.py
@@ -38,11 +38,11 @@ class Catalogo:
 
         # Intentamos seleccionar la base de datos
         try:
-            self.cursor.execute(f"USE {database}")
+            self.cursor.execute(f"USE {database};")
         except mysql.connector.Error as err:
             # Si la base de datos no existe, la creamos
             if err.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
-                self.cursor.execute(f"CREATE DATABASE {database}")
+                self.cursor.execute(f"CREATE DATABASE {database};")
                 self.conn.database = database
             else:
                 raise err
@@ -54,7 +54,7 @@ class Catalogo:
             cantidad INT NOT NULL,
             precio DECIMAL(10, 2) NOT NULL,
             imagen_url VARCHAR(255),
-            proveedor INT)''')
+            proveedor INT);''')
         self.conn.commit()
 
         # Cerrar el cursor inicial y abrir uno nuevo con el parámetro dictionary=True
@@ -64,12 +64,12 @@ class Catalogo:
     #----------------------------------------------------------------
     def agregar_producto(self, codigo, descripcion, cantidad, precio, proveedor):
         # Verificamos si ya existe un producto con el mismo código
-        self.cursor.execute(f"SELECT * FROM productos WHERE codigo = {codigo}")
+        self.cursor.execute(f"SELECT * FROM productos WHERE codigo = {codigo};")
         producto_existe = self.cursor.fetchone()
         if producto_existe:
             return False
 
-        sql = "INSERT INTO productos (codigo, descripcion, cantidad, precio, proveedor) VALUES (%s, %s, %s, %s, %s)"
+        sql = "INSERT INTO productos (codigo, descripcion, cantidad, precio, proveedor) VALUES (%s, %s, %s, %s, %s);"
         valores = (codigo, descripcion, cantidad, precio, proveedor)
 
         self.cursor.execute(sql, valores)        
@@ -80,12 +80,12 @@ class Catalogo:
 
     def consultar_producto(self, codigo):
         # Consultamos un producto a partir de su código
-        self.cursor.execute(f"SELECT * FROM productos WHERE codigo = {codigo}")
+        self.cursor.execute(f"SELECT * FROM productos WHERE codigo = {codigo};")
         return self.cursor.fetchone()
 
     #----------------------------------------------------------------
     def modificar_producto(self, codigo, nueva_descripcion, nueva_cantidad, nuevo_precio, nuevo_proveedor):
-        sql = "UPDATE productos SET descripcion = %s, cantidad = %s, precio = %s, proveedor = %s WHERE codigo = %s"
+        sql = "UPDATE productos SET descripcion = %s, cantidad = %s, precio = %s, proveedor = %s WHERE codigo = %s;"
         valores = (nueva_descripcion, nueva_cantidad, nuevo_precio, nuevo_proveedor, codigo)
         self.cursor.execute(sql, valores)
         self.conn.commit()
@@ -95,16 +95,17 @@ class Catalogo:
     
     #----------------------------------------------------------------
     def listar_productos(self):
-        self.cursor.execute("SELECT * FROM productos")
+        self.cursor.execute("SELECT * FROM productos;")
         productos = self.cursor.fetchall()
         return productos
 
     #----------------------------------------------------------------
     def eliminar_producto(self, codigo):
         # Eliminamos un producto de la tabla a partir de su código
-        self.cursor.execute(f"DELETE FROM productos WHERE codigo = {codigo}")
+        
+        self.cursor.execute(f"DELETE FROM productos WHERE codigo = {codigo};")
         self.conn.commit()
-        return self.cursor.rowcount > 0
+        return True
 
     #----------------------------------------------------------------
     def mostrar_producto(self, codigo):
@@ -132,12 +133,16 @@ def index():
 
 
 @app.route('/productos')
-def productos():
+def productos_html():
     return render_template('productos.html')
 
-# Crear una instancia de la clase Catalogo
+@app.route('/modificar_producto')
+def modificar_producto_html():
+    return render_template('modificar_producto.html')
 
-catalogo = Catalogo(host='localhost', user='root', password='Rodrigo2023', database='miapp')
+@app.route('/borrar_producto')
+def borrar_producto_html():
+    return render_template('borrar_producto.html')
 
 #print(catalogo.modificar_producto(2, 'osito', 10, 2332, 5))
 
@@ -178,24 +183,22 @@ def agregar_producto():
         return jsonify({"mensaje": "Producto ya existe"}), 400
 
 #--------------------------------------------------------------------
-@app.route("/productos/<int:codigo>", methods=["PUT"])
-def modificar_producto(codigo):
+@app.route("/modificar_producto", methods=["PUT"])
+def modificar_producto():
     try:
-        print(f"DEBUG: Código recibido: {codigo}")
-
-        nueva_descripcion = request.form.get('descripcion')
-        nueva_cantidad = request.form.get('cantidad')
-        nuevo_precio = request.form.get('precio')
-        nuevo_proveedor = request.form.get('proveedor')
+        
+        codigo = request.form.get('codigo_modificar')
+        print(codigo)
+        nueva_descripcion = request.form.get('nueva_descripcion')
+        nueva_cantidad = request.form.get('nueva_cantidad')
+        nuevo_precio = request.form.get('nuevo_precio')
+        nuevo_proveedor = request.form.get('nuevo_proveedor')
 
         # Validar los datos de entrada
         if not codigo or not nueva_cantidad or not nuevo_precio or not nuevo_proveedor:
             return jsonify({"mensaje": "Datos de entrada incompletos"}), 400
 
-        # Convertir valores según sea necesario (por ejemplo, a números)
-        nueva_cantidad = int(nueva_cantidad)
-        nuevo_precio = float(nuevo_precio)
-        nuevo_proveedor = int(nuevo_proveedor)
+        
 
         if catalogo.modificar_producto(codigo, nueva_descripcion, nueva_cantidad, nuevo_precio, nuevo_proveedor):
             return jsonify({"mensaje": "Producto modificado"}), 200
@@ -210,26 +213,21 @@ def modificar_producto(codigo):
 
 #--------------------------------------------------------------------
 
-@app.route("/productos/<int:codigo>", methods=["DELETE"])
-def eliminar_producto(codigo):
-    # Primero, obtén la información del producto para encontrar la imagen
-    producto = catalogo.consultar_producto(codigo)
-    if producto:
-        # Eliminar la imagen asociada si existe
-        ruta_imagen = os.path.join(ruta_destino, producto['imagen_url'])
-        if os.path.exists(ruta_imagen):
-            os.remove(ruta_imagen)
+@app.route("/borrar_producto/<int:codigo>", methods=["POST"])
+def borrar_producto(codigo):
+    producto_existente = catalogo.consultar_producto(codigo)
 
-        # Luego, elimina el producto del catálogo
-        if catalogo.eliminar_producto(codigo):
-            return jsonify({"mensaje": "Producto eliminado"}), 200
-        else:
-            return jsonify({"mensaje": "Error al eliminar el producto"}), 500
+    if producto_existente:
+        catalogo.borrar_producto(codigo)
+        return jsonify({"mensaje": "Producto eliminado"}), 200
     else:
         return jsonify({"mensaje": "Producto no encontrado"}), 404
+
+    #jsonify({"mensaje": "Error al eliminar el producto"}), 404
+   
 #--------------------------------------------------------------------
 
-
+catalogo = Catalogo(host='localhost', user='root', password='Rodrigo2023', database='miapp')
 
 if __name__ == "__main__":
     app.run(debug=True)
